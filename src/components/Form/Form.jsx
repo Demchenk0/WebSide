1ee52c327007e4771ffe6d4105055507c28cc723
Demchenk0 <santadyr2016@gmail.com>
2024-01-21 22:46:28 +0200

    Form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -5,12 +5,14 @@ const Form = () => {
 	return (
 		<section className={s.section}>
 			<div className={s.container}>
+			<div>
 				<h1 className={s.title}>Залиште заявку</h1>
 				<p className={s.text}>Заповнюйте форму і ми зв’яжемось з вами</p>
-				<div>
+			</div>
+				<div className={s.boxForm}>
 					<form action="" className={s.form}>
 						<input type="text" name="name" placeholder="Ваше Ім’я" required className={s.input}/>
-						<input type="text" name="phone" placeholder="+380" required className={s.input}/>
+						<input type="number" name="phone" placeholder="+380" required className={s.input}/>
 						<button className={`${s.button} ${s.animate}`}>
 							Залишити заявку
 						</button>
